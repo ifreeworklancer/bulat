c3e57759c3e4898581e004212bbf5d47ac430d82
Oleh Talanoff <talanov.o@gmail.com>
2019-02-01 21:41:35 +0200

    #

--- app/Services/CustomPathGenerator.php
+++ app/Services/CustomPathGenerator.php
@@ -7,21 +7,37 @@ use Spatie\MediaLibrary\PathGenerator\PathGenerator;
 
 class CustomPathGenerator implements PathGenerator
 {
+	/**
+	 * @param Media $media
+	 * @return string
+	 */
 	public function getPath(Media $media): string
 	{
 		return $this->getBasePath($media) . '/' . $this->hash($media) . '/';
 	}
 
+	/**
+	 * @param Media $media
+	 * @return string
+	 */
 	public function getPathForConversions(Media $media): string
 	{
 		return $this->getBasePath($media) . '/' . $this->hash($media) . '/';
 	}
 
+	/**
+	 * @param Media $media
+	 * @return string
+	 */
 	public function getPathForResponsiveImages(Media $media): string
 	{
 		return $this->getBasePath($media) . '/' . $this->hash($media) . '/r/';
 	}
 
+	/**
+	 * @param Media $media
+	 * @return string
+	 */
 	protected function getBasePath(Media $media): string
 	{
 		return $media->collection_name;

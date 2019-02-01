a106b0cca57faec76d6b7d116eee489b0c875224
Oleh Talanoff <talanov.o@gmail.com>
2019-02-01 18:58:06 +0200

    #

--- app/Services/CustomPathGenerator.php
+++ app/Services/CustomPathGenerator.php
@@ -9,21 +9,30 @@ class CustomPathGenerator implements PathGenerator
 {
 	public function getPath(Media $media): string
 	{
-		return $this->getBasePath($media) . '/' . $media->id . '/';
+		return $this->getBasePath($media) . '/' . $this->hash($media) . '/';
 	}
 
 	public function getPathForConversions(Media $media): string
 	{
-		return $this->getBasePath($media) . '/' . $media->id . '/';
+		return $this->getBasePath($media) . '/' . $this->hash($media) . '/';
 	}
 
 	public function getPathForResponsiveImages(Media $media): string
 	{
-		return $this->getBasePath($media) . '/' . $media->id . '/r/';
+		return $this->getBasePath($media) . '/' . $this->hash($media) . '/r/';
 	}
 
 	protected function getBasePath(Media $media): string
 	{
 		return $media->collection_name;
+	}
+
+	/**
+	 * @param Media $media
+	 * @return bool|string
+	 */
+	private function hash(Media $media)
+	{
+		return substr(md5($media->id), 0, 6);
 	}
 }

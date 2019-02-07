6d4a1ad6e2af1bbfee025e6f56fd0cb94e903d3c
Oleh Talanoff <talanov.o@gmail.com>
2019-02-07 12:55:25 +0200

    Modals, mails

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Models\User\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+		'phone' => $faker->phoneNumber,
         'password' => bcrypt('password'),
         'remember_token' => str_random(10),
     ];

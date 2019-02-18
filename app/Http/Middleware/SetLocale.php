6372bcf0352e0c729b95cf965f5839027b1fcba0
Oleh Talanoff <talanov.o@gmail.com>
2019-02-18 16:13:43 +0200

    #

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -15,28 +15,30 @@ class SetLocale
      */
     public function handle($request, Closure $next)
     {
-		if (session()->has('locale')) {
-			$locale = session()->get('locale', config('app.locale'));
-		} else {
-			$locale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
-
-			if ($locale !== 'ru' && $locale !== 'en') {
-				$locale = 'ru';
-			}
-		}
+//		if (session()->has('locale')) {
+//			$locale = session()->get('locale', config('app.locale'));
+//		} else {
+//			$locale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+//
+//			if ($locale !== 'ru' && $locale !== 'en') {
+//				$locale = 'ru';
+//			}
+//		}
+//
+//		if (app('router')->currentRouteNamed('admin.*')) {
+//			app()->setLocale('ru');
+//		} else {
+//			app()->setLocale($locale);
+//		}
+//
+//		if (app()->getLocale() === 'ru') {
+//			setlocale(LC_TIME, 'ru_RU.utf-8');
+//		}
 
 		if (app('router')->currentRouteNamed('admin.*')) {
 			app()->setLocale('ru');
 		} else {
-			app()->setLocale($locale);
-		}
-
-		if (app()->getLocale() === 'ru') {
-			setlocale(LC_TIME, 'ru_RU.utf-8');
-		}
-
-		if (app('router')->currentRouteNamed('admin.*')) {
-			app()->setLocale('ru');
+			app()->setLocale('en');
 		}
 
 		return $next($request);

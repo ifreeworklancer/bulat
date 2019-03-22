bc6dc14a0fadd288c5c2e0ae68c130ed755a4cb6
Oleh Talanoff <talanov.o@gmail.com>
2019-03-22 13:22:15 +0200

    Pre-production

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,21 +11,9 @@ class UsersTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$faker = \Faker\Factory::create();
-
-		factory(\App\Models\User\User::class)->create(['email' => 'admin@app.com', 'role_id' => 1]);
-		factory(\App\Models\User\User::class, 10)->create();
-
-		foreach (\App\Models\User\User::get() as $user) {
-			if (rand(0, 1)) {
-				$user->profile()->create([
-					'phone_1' => $faker->e164PhoneNumber,
-					'phone_2' => rand(0, 1) ? $faker->e164PhoneNumber : null,
-					'country' => $faker->country,
-					'city' => $faker->city,
-					'address' => rand(0, 1) ? $faker->address : null,
-				]);
-			}
-		}
+		factory(\App\Models\User\User::class)->create([
+			'email' => 'admin@app.com',
+			'role_id' => 1,
+		]);
 	}
 }

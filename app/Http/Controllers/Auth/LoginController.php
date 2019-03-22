ef8f990a187b5d412484ef1ec4713c571790e4af
Oleh Talanoff <talanov.o@gmail.com>
2019-03-22 13:06:32 +0200

    Pre-production

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
 
 	public function showLoginForm()
 	{
-		$image = Media::where('collection_name', 'category')->inRandomOrder()->first()->getFullUrl();
+		$image = optinal(Media::where('collection_name', 'category')->inRandomOrder()->first())->getFullUrl();
 		return view('auth.login', compact('image'));
 	}
 }

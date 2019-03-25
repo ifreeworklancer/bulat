ddd8f8760d58fdcd4b8b3a36bf53bcf1fbaaab18
Oleh Talanoff <talanov.o@gmail.com>
2019-03-25 17:19:12 +0200

    Pre-production

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -50,7 +50,8 @@
                            class="btn btn-warning btn-squire">
                             <i class="i-pencil"></i>
                         </a>
-                        <button class="btn btn-danger btn-squire">
+                        <button class="btn btn-danger btn-squire"
+                                onclick="deleteItem('{{ route('admin.tags.destroy', $tag) }}')">
                             <i class="i-trash"></i>
                         </button>
                     </td>
@@ -74,3 +75,22 @@
     </section>
 
 @endsection
+
+@push('scripts')
+    <form method="post" id="delete" style="display: none">
+        @csrf
+        @method('delete')
+    </form>
+
+    <script>
+      function deleteItem(route) {
+        const form = document.getElementById('delete');
+        const conf = confirm('Уверены?');
+
+        if (conf) {
+          form.action = route;
+          form.submit();
+        }
+      }
+    </script>
+@endpush

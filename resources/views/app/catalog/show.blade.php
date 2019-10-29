3f25be6a90384641cb02f0c6eeced57130791834
Dmyto Haidash <dmyto.haidash@gmail.com>
2019-10-29 12:40:53 +0200

    sky

--- resources/views/app/catalog/show.blade.php
+++ resources/views/app/catalog/show.blade.php
@@ -50,9 +50,6 @@
                 <div class="col-md-6 pl-md-4">
                     <div class="d-flex flex-column flex-lg-row align-items-start align-items-lg-end mb-4">
                         <div>
-                            <small class="text-muted mr-3">@lang('pages.product.sku'):</small>
-                            {{ $product->sku }}
-
                             <h4 class="price mt-4">
                                 <small class="text-muted">@lang('pages.product.price'):</small>
                                 {{ number_format($product->price, 0, ',', ' ') }}
@@ -133,14 +130,14 @@
 
 @push('scripts')
     <script>
-        function togglefavorites() {
-            const el = event.target;
-            event.preventDefault();
+      function togglefavorites() {
+        const el = event.target;
+        event.preventDefault();
 
-            axios.post('{{ route('app.catalog.favorites', $product) }}')
-                .then(function (response) {
-                    el.innerText = (response.data.status === 'added' ? 'star' : 'star_border');
-                });
-        }
+        axios.post('{{ route('app.catalog.favorites', $product) }}')
+          .then(function (response) {
+            el.innerText = (response.data.status === 'added' ? 'star' : 'star_border');
+          });
+      }
     </script>
 @endpush

861697f9486953b9865fa574fee8eacc2581ebad
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-07 13:13:48 +0200

    new catalog filter

--- resources/views/partials/app/catalog/preview.blade.php
+++ resources/views/partials/app/catalog/preview.blade.php
@@ -5,23 +5,30 @@
         <div class="product-preview__info p-3">
             <div class="mb-auto">
                 <h5 class="mb-1">{{ $product->translate('title') }}</h5>
-                @if ($product->in_stock)
+              {{--  @if ($product->in_stock)
                     <p class="text-success">@lang('pages.product.in_stock')</p>
                 @else
                     <p class="text-danger">@lang('pages.product.out_of_stock')</p>
-                @endif
+                @endif--}}
 
                {{-- @if ($product->hasTranslation('description'))
                     <p class="smaller text-muted mb-1">{{ str_limit($product->translate('description'), 50) }}</p>
                 @endif--}}
             </div>
+            <div class="justify-content-between">
+                <h6 class="mb-0 text-primary">
+                    {{ $product->price }}
+                    <small class="text-uppercase currency">
+                        @lang('common.currency')
+                    </small>
+                </h6>
+                @if ($product->in_stock)
+                    <p class="text-success">@lang('pages.product.in_stock')</p>
+                @else
+                    <p class="text-danger">@lang('pages.product.out_of_stock')</p>
+                @endif
+            </div>
 
-            <h6 class="mb-0 text-primary">
-                {{ $product->price }}
-                <small class="text-uppercase currency">
-                    @lang('common.currency')
-                </small>
-            </h6>
         </div>
     </a>
 </article>

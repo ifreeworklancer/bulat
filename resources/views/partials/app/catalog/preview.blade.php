5e08ed51964c5adaf68461fb7f2b17ef5e8a1187
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-07 13:10:22 +0200

    new catalog filter

--- resources/views/partials/app/catalog/preview.blade.php
+++ resources/views/partials/app/catalog/preview.blade.php
@@ -3,6 +3,18 @@
         <figure class="product-preview__image lozad"
                 data-background-image="{{ $product->preview }}"></figure>
         <div class="product-preview__info p-3">
+            <div class="mb-auto">
+                <h5 class="mb-1">{{ $product->translate('title') }}</h5>
+                @if ($product->in_stock)
+                    <p class="text-success">@lang('pages.product.in_stock')</p>
+                @else
+                    <p class="text-danger">@lang('pages.product.out_of_stock')</p>
+                @endif
+
+               {{-- @if ($product->hasTranslation('description'))
+                    <p class="smaller text-muted mb-1">{{ str_limit($product->translate('description'), 50) }}</p>
+                @endif--}}
+            </div>
 
             <h6 class="mb-0 text-primary">
                 {{ $product->price }}
@@ -10,19 +22,6 @@
                     @lang('common.currency')
                 </small>
             </h6>
-
-            <h5 class="mb-1">{{ $product->translate('title') }}</h5>
-            @if ($product->in_stock)
-                <p class="text-success">@lang('pages.product.in_stock')</p>
-            @else
-                <p class="text-danger">@lang('pages.product.out_of_stock')</p>
-            @endif
-
-            {{-- @if ($product->hasTranslation('description'))
-                 <p class="smaller text-muted mb-1">{{ str_limit($product->translate('description'), 50) }}</p>
-             @endif--}}
-
-
         </div>
     </a>
 </article>
